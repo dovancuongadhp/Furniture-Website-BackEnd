f9b97d3ec019aa7993d64fa361d4d0ee2d8a00db
vuduynhiennn <vuduynhiencute@gmail.com>
2023-03-04 23:01:12 +0700

    s

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Furniture-Website-BackEnd
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,4 +4,7 @@ export default function appRoutes(app:Application){
     app.use("/sayHi",(req:any,res:any)=>{
         res.send("Say Hi")
     })
+    app.use("/hi", (req:any, res:any) => {
+        res.send("I saw your message and reply: hi")
+    })
 }
