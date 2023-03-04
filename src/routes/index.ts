6b72e48896ffa4e56d9abdbf6ad7e578dbef2a7f
vuduynhiennn <vuduynhiencute@gmail.com>
2023-03-04 22:53:55 +0700

    vuduynhien message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,4 +4,8 @@ export default function appRoutes(app:Application){
     app.use("/sayHi",(req:any,res:any)=>{
         res.send("Say Hi")
     })
+
+    app.use("/responseHi", (req:any, res:any) => {
+        res.send("Vu Duy Nhien saw your message and He says hii")
+    })
 }
